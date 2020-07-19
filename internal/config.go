78a22b0de2297a02aff867104fcda553e9297fb4
Dylan Hackworth <dhpf@pm.me>
2020-07-19 13:57:39 -0500

    Added config.go

---
+++ internal/config.go
@@ -0,0 +1,36 @@
+package internal
+
+import (
+	util "github.com/Floor-Gang/utilpkg/config"
+	"log"
+)
+
+type Config struct {
+	Auth     string            `yaml:"auth_server"`
+	Prefix   string            `yaml:"prefix"`
+	Channels map[string]string `yaml:"linked_channels"`
+}
+
+const configPath = "./config.yml"
+
+func GetConfig() Config {
+	config := Config{
+		Auth:     "",
+		Prefix:   ".link",
+		Channels: make(map[string]string),
+	}
+
+	err := util.GetConfig(configPath, &config)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return config
+}
+
+func (config *Config) Save() {
+	if err := util.Save(configPath, config); err != nil {
+		log.Fatalln(err)
+	}
+}
